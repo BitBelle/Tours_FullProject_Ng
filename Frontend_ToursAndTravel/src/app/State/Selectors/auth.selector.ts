ab68eff78e3c652942808c292b3c45223431bbd7
BitBelle <wainainamercy432@gmail.com>
2024-08-15 12:03:38 +0300

    Made some modifications and deleted some files

--- Frontend_ToursAndTravel/src/app/State/Selectors/auth.selector.ts
+++
@@ -1,8 +0,0 @@
-import { createFeatureSelector, createSelector } from "@ngrx/store";
-import { AuthInterface } from "../Reducers/auth.reducer";
-
-
-
-const authselectorFeature = createFeatureSelector<AuthInterface>('auth')
-
-export const errorSelector= createSelector(authselectorFeature, (state)=>state.loginErrorMessage)
